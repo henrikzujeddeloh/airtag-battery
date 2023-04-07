181edabc4264834371f9745a2a8a1a9c5f863af7
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-04-07 14:34:29 +0200

    added comments and updated README

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # AirTagBattery
 
-Apple removed the battery status of AirTags from the FindMy app. This script reads the battery status from the FindMy cache for all AirTags.
+Apple removed the battery status of AirTags from the FindMy app. This script reads the battery status from the FindMy cache for all AirTags and displays battery level.
+
 
 ## Usage
 
+Make sure the terminal has full disk access:
+System Settings > Privacy & Security > Full Disk Acess > allow for Terminal (or iTerm)
 
+Run `python bat.py`
--- bat.py
+++ bat.py
@@ -1,20 +1,21 @@
 import json
 import os
-from tabulate import tabulate
+import tabulate
 
+# Location of FindMy cache file for AirTags
 FINDMY_FILES = '~/Library/Caches/com.apple.findmy.fmipcore/Items.data'
 
-keys = 'batteryStatus'
-
+# dict with AirTag names, and battery status
 airtags = {"AirTag" : [], "BatteryStatus" : [], "BatteryLevel" : []}
 
+# opens file and reads as json
 f = open(os.path.expanduser(FINDMY_FILES), 'r')
 json_data = json.loads(f.read())
 for item in json_data:
     airtags["AirTag"].append(item["name"])
     airtags["BatteryStatus"].append(item["batteryStatus"])
 
-
+# Loop to translate battery status to battery level
 i=0
 for tag in airtags["AirTag"]:
     bat_stat = airtags["BatteryStatus"][i]
@@ -31,5 +32,6 @@ for tag in airtags["AirTag"]:
         airtags["BatteryLevel"].append('#')
 
 
+# creates final table to output
 headers = ["AirTag", "Battery Status", "Battery Level"]
-print(tabulate(airtags, headers = headers, tablefmt='outline'))
+print(tabulate.tabulate(airtags, headers = headers, tablefmt='outline'))
