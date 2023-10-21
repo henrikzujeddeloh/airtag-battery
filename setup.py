5876e7028a81aa4163db9441a3dcddbd49cdc6e8
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-10-21 09:18:16 +0200

    updated: created python package with CLI command

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    # name = "airtag-bat",
+    # version = "1.0.0",
+    # description = "A tool to read AirTag battery status",
+    # author = "Henrik zu Jeddeloh",
+    # author_email = "henrik.zujeddeloh@protonmail.com",
+    # url = "https://github.com/henrikzujeddeloh/AirTagBattery" ,
+    # packages = find_packages(),
+    # install_requires = ["tabulate"],
+    # entry_points={
+    #     'console_scripts': 'airtag-bat=airtag-bat:get_bat',
+    # },
+)
